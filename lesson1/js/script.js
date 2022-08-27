5de0f5306283dc3155b212f5498e68506c01fb6c
Vladimir <writer79@mail.ru>
2022-08-27 22:46:43 +0700

    lesson two is ready

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -16,7 +16,6 @@ const renderProduct = (obj) => {
 };
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item));
-    console.log(productsList);
     document.querySelector('.products').innerHTML = productsList.join('');
 };
 
